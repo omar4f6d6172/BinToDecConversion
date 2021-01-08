f8bd1eaf8d663cb67f3c14c31310a76e53edfa6b
rino2000 <rino.RH35@Gmail.com>
2021-01-08 19:52:35 +0100

    Update BinToDecConversion.java

--- BinToDecConversion.java
+++ BinToDecConversion.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 public class BinToDecConversion {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
@@ -12,19 +11,18 @@ public class BinToDecConversion {
         for(int i=0;i<y.length;i++){
             System.out.print("Enter binary values " );
             y[i] = sc.nextInt();
-            if(y[i] ==1 || y[i] ==0){
+        if(y[i] ==1 || y[i] ==0){
             o[i]= y[i] * (int) Math.pow(2, i);
-            }
-            else{
+        }
+        else{
             System.out.println("Values are rejected");
-            break;
+         break;
             }
         }
         int w = Arrays.stream(o).sum();
         System.out.println("the sum Result is: " + w);
         System.out.print("please enter (y Key + Enter Key) if you want to enter an other value , or press (any key + Enter key) to finish ");
         p=sc.next().charAt(0);
-    
+        }
     }
-}
 }
